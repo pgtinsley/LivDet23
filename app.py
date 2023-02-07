d03dcb17b4595dbe8e4f01ad3a4968ab4182df68
Patrick Tinsley <patrick.g.tinsley@gmail.com>
2023-02-07 02:03:51 -0500

    see if it works

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
-from flask import Flask
+
+import datetime
+
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return render_template(
+    'index.html',
+    utc_dt = datetime.datetime.utcnow()
+    )
